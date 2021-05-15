56cceb9c18694d3ff6cf14db5ea0b72daa6047a8
xeusteerapat <xeus085@gmail.com>
2021-05-15 16:46:16 +0700

    line break

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
             />
             <h1 className='mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl'>
               You can sleep from anywhere.
-              <br className='hidden lg:inline' />
+              <br />
               <span className='text-indigo-500'>Take advantage of it.</span>
             </h1>
             <p className='mt-2 text-gray-600 sm:mt-4 sm:text-xl'>
